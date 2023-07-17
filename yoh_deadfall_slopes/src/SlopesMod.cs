6711240d47b3498758a457d05fb57fb2b0aeddbb
Yoh Deadfall <yoh.deadfall@hotmail.com>
2023-07-17 23:28:10 +0300

    Added smooth slopes mod

---
+++ yoh_deadfall_slopes/src/SlopesMod.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using VoxelTycoon;
+using VoxelTycoon.Modding;
+using VoxelTycoon.Tools.TrackBuilder;
+
+namespace Yoh.VoxelTycoon.Slopes;
+
+public sealed class SlopesMod : Mod
+{
+    protected override void Deinitialize()
+    {
+		base.Deinitialize();
+
+		var multiplier = WorldSettings.Current.GetInt<SlopesModSettings>(SlopesModSettings.RailSlopeLengthMultiplier);
+
+		UpdateSlope(RailPathfinder.Slope);
+		UpdateSlope(RailPathfinder.SlopeRotated);
+		
+		UpdateSlope(RailPathfinder.SlopeIn);
+		UpdateSlope(RailPathfinder.SlopeInRotated);
+		
+		UpdateSlope(RailPathfinder.SlopeInOut);
+		UpdateSlope(RailPathfinder.SlopeInOutRotated);
+
+		UpdateSlope(RailPathfinder.SlopeOut);
+		UpdateSlope(RailPathfinder.SlopeOutRotated);
+
+		void UpdateSlope(NodeType node)
+		{
+			node.Width /= multiplier;
+			node.Offset /= multiplier;
+		}
+
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeIn, static (ref Vector3 point) => ref point.x);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeIn, static (ref Vector3 point) => ref point.z);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeInOut, static (ref Vector3 point) => ref point.x);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeInOut, static (ref Vector3 point) => ref point.x);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeOut, static (ref Vector3 point) => ref point.z);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeOut, static (ref Vector3 point) => ref point.z);
+
+		UpdateCurve(R.Curves.Rails.StraightSlopeIn, static (ref Vector3 point) => ref point.z);
+		UpdateCurve(R.Curves.Rails.StraightSlopeInOut, static (ref Vector3 point) => ref point.z);
+		UpdateCurve(R.Curves.Rails.StraightSlopeOut, static (ref Vector3 point) => ref point.z);
+
+		void UpdateCurve(Curve curve, GetCoordinateReference getCoordinate) =>
+			UpdateCurveBy(1.0f / multiplier, curve, getCoordinate);
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+		var multiplier = WorldSettings.Current.GetInt<SlopesModSettings>(SlopesModSettings.RailSlopeLengthMultiplier);
+
+		UpdateSlope(RailPathfinder.Slope);
+		UpdateSlope(RailPathfinder.SlopeRotated);
+		
+		UpdateSlope(RailPathfinder.SlopeIn);
+		UpdateSlope(RailPathfinder.SlopeInRotated);
+		
+		UpdateSlope(RailPathfinder.SlopeInOut);
+		UpdateSlope(RailPathfinder.SlopeInOutRotated);
+
+		UpdateSlope(RailPathfinder.SlopeOut);
+		UpdateSlope(RailPathfinder.SlopeOutRotated);
+
+		void UpdateSlope(NodeType node)
+		{
+			node.Width *= multiplier;
+			node.Offset *= multiplier;
+		}
+
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeIn, static (ref Vector3 point) => ref point.x);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeIn, static (ref Vector3 point) => ref point.z);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeInOut, static (ref Vector3 point) => ref point.x);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeInOut, static (ref Vector3 point) => ref point.z);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeOut, static (ref Vector3 point) => ref point.x);
+		UpdateCurve(R.Curves.Rails.DiagonalSlopeOut, static (ref Vector3 point) => ref point.z);
+
+		UpdateCurve(R.Curves.Rails.StraightSlopeIn, static (ref Vector3 point) => ref point.z);
+		UpdateCurve(R.Curves.Rails.StraightSlopeInOut, static (ref Vector3 point) => ref point.z);
+		UpdateCurve(R.Curves.Rails.StraightSlopeOut, static (ref Vector3 point) => ref point.z);
+
+		void UpdateCurve(Curve curve, GetCoordinateReference getCoordinate) =>
+			UpdateCurveBy(multiplier, curve, getCoordinate);
+	}
+
+	private delegate ref float GetCoordinateReference(ref Vector3 point);
+
+	private static void UpdateCurveBy(float multiplier, Curve curve, GetCoordinateReference getCoordinate)
+	{
+		var points = curve.Points;
+		
+		for (var index = 0; index < points.Length; index += 1)
+			getCoordinate(ref points[index]) *= multiplier;
+
+		curve.RecalculateLength();
+	}
+}
---
+++ yoh_deadfall_slopes/src/SlopesModSettings.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using VoxelTycoon;
+using VoxelTycoon.Game.UI;
+using VoxelTycoon.Modding;
+using static VoxelTycoon.Game.UI.SettingsWindowUpDownItem;
+
+namespace Yoh.VoxelTycoon.Slopes;
+
+public sealed class SlopesModSettings : SettingsMod
+{
+	public const string RailSlopeLengthMultiplier = nameof(RailSlopeLengthMultiplier);
+
+	protected override void SetDefaults(WorldSettings world) =>
+		world.SetInt<SlopesModSettings>(RailSlopeLengthMultiplier, value: 1);
+
+	protected override void SetupSettingsControl(SettingsControl control, WorldSettings world) =>
+		control.AddUpDown(
+			name: "Rail Slope Length Multiplier",
+			description: "Increasing the slope length makes them more realistic and smooth, but requires more space to build them.",
+			world.GetInt<SlopesModSettings>(RailSlopeLengthMultiplier).ToString(),
+			Enumerable
+				.Range(1, 3)
+				.Select(
+					value => new Option
+					{
+						Name = value.ToString(),
+						Setter = () => world.SetInt<SlopesModSettings>(RailSlopeLengthMultiplier, value)
+					})
+				.ToArray());
+	}
